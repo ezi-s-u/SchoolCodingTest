d42af19559510293a2831a5c33b40541cd7d4a48
leejisu <ljsgoo0617@gmail.com>
2023-02-15 20:38:12 +0900

    배열 생성 및 입력

--- CodingTest/src/CodingTestSuhaeng/suhaeng1.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng1.java
@@ -1,9 +1,21 @@
 package CodingTestSuhaeng;
 
+import java.util.Scanner;
+
 public class suhaeng1 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int A[] = new int[5];
+		int B[] = new int[5];
+		
+		for(int i = 0; i < 5; i++) {
+			A[i] = sc.nextInt();
+		}
+		for(int i = 0; i < 5; i++) {
+			B[i] = sc.nextInt();
+		}
 	}
 
 }
