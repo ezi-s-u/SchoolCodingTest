ad4c414aaa6a72af86d7300b4f55cf934af12677
leejisu <ljsgoo0617@gmail.com>
2023-02-15 20:43:14 +0900

    오름차순 정렬

--- CodingTest/src/CodingTestSuhaeng/suhaeng1.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng1.java
@@ -1,6 +1,7 @@
 package CodingTestSuhaeng;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class suhaeng1 {
 
@@ -9,6 +10,7 @@ public class suhaeng1 {
 		
 		int A[] = new int[5];
 		int B[] = new int[5];
+		int res[] = new int[10];
 		
 		for(int i = 0; i < 5; i++) {
 			A[i] = sc.nextInt();
@@ -16,6 +18,15 @@ public class suhaeng1 {
 		for(int i = 0; i < 5; i++) {
 			B[i] = sc.nextInt();
 		}
+		
+		for(int i = 0; i < 5; i++) {
+			res[i] = A[i];
+		}
+		for(int i = 5; i < res.length; i++) {
+			res[i] = B[i-5];
+		}
+		
+		Arrays.sort(res);
 	}
 
 }
