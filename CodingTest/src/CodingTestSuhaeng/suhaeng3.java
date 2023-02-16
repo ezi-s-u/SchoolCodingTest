56f63c3d68cbfd2e3ae1a3016666f4763ea0d2b1
leejisu <ljsgoo0617@gmail.com>
2023-02-16 18:00:28 +0900

    Scanner 세팅, 입력받기, 부채꼴 클래스 세팅

--- CodingTest/src/CodingTestSuhaeng/suhaeng3.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng3.java
@@ -1,10 +1,24 @@
 package CodingTestSuhaeng;
 
+import java.util.Scanner;
+
 public class suhaeng3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		buchaeggol b = new buchaeggol();
+		System.out.print("반지름 입력 : ");
+		double rad = sc.nextDouble();
 	}
 
+}
+
+class buchaeggol {
+	double area(double rad) {
+		return (rad*rad*3.14)/4;
+	}
+	double circum(double rad) {
+		return (rad*2*3.14)/4;
+	}
 }
