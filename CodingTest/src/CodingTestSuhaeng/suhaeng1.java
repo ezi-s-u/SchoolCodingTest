3c55fbf7f3d2005b8053c5c3c78b95893073aab9
leejisu <ljsgoo0617@gmail.com>
2023-02-16 17:12:30 +0900

    중복제거 후 출력 (완성)

--- CodingTest/src/CodingTestSuhaeng/suhaeng1.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng1.java
@@ -12,16 +12,28 @@ public class suhaeng1 {
 		int B[] = new int[5];
 		int res[] = new int[10];
 		
+		System.out.print("첫번째 배열의 데이타 : ");
 		for(int i = 0; i < 5; i++) {
 			A[i] = sc.nextInt();
 			res[i] = A[i];
 		}
+		
+		System.out.print("두번째 배열의 데이타 : ");
 		for(int i = 0; i < 5; i++) {
 			B[i] = sc.nextInt();
 			res[i+5] = B[i];
 		}
 		
 		Arrays.sort(res);
+		
+		System.out.print("병합된 배열의 데이타 : ");
+		for(int i = 0; i < res.length; i++) {
+			if(i > 0 && res[i-1]!=res[i]) {
+				System.out.print(res[i]+" ");
+			} else if(i == 0) {
+				System.out.print(res[i]+" ");
+			}
+		}
 	}
 
 }
