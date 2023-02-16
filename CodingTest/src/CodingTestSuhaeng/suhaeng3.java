b65785f1f8dc45a056364826f86d5fff858ddb6d
leejisu <ljsgoo0617@gmail.com>
2023-02-16 18:03:37 +0900

    출력 (둘레 값 오답)

--- CodingTest/src/CodingTestSuhaeng/suhaeng3.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng3.java
@@ -10,6 +10,8 @@ public class suhaeng3 {
 		buchaeggol b = new buchaeggol();
 		System.out.print("반지름 입력 : ");
 		double rad = sc.nextDouble();
+		
+		System.out.println("반지름 "+rad+"인 부채꼴의 면적 : "+b.area(rad)+" , 둘레 : "+b.circum(rad));
 	}
 
 }
