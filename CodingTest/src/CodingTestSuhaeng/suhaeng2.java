01c44782bfba55977d9e42bb5a0f589a3cd94e88
leejisu <ljsgoo0617@gmail.com>
2023-02-27 15:37:17 +0900

    조건 1, 4, 5 충족

--- CodingTest/src/CodingTestSuhaeng/suhaeng2.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng2.java
@@ -6,7 +6,24 @@ public class suhaeng2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		
+		System.out.print("학생수 입력 : ");
+		int per = sc.nextInt();
+		System.out.println("");
+		
+		int score[][] = new int[per][3];
+		
+		for(int i = 0; i < per; i++) {
+			for(int j = 0; j < score[i].length; j++) {
+				System.out.print("점수 "+(j+1)+" 입력 : ");
+				score[i][j] = sc.nextInt();
+				while(score[i][j]>100 || score[i][j]<0) {
+					System.out.print("다시 입력하세요==> 점수 "+(j+1)+" 입력 : ");
+					score[i][j] = sc.nextInt();
+				}
+			}
+			System.out.println("");
+		}
 	}
 
 }
