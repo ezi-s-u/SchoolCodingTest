c9076469483f510a84294192463d944acef41164
leejisu <ljsgoo0617@gmail.com>
2023-02-27 16:33:37 +0900

    평점 제외한 출력까지

--- CodingTest/src/CodingTestSuhaeng/suhaeng2.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng2.java
@@ -11,19 +11,32 @@ public class suhaeng2 {
 		int per = sc.nextInt();
 		System.out.println("");
 		
-		int score[][] = new int[per][3];
-		
+		int score[][] = new int[per][4];
+		int max_sco[] = new int[per];
 		for(int i = 0; i < per; i++) {
-			for(int j = 0; j < score[i].length; j++) {
+			for(int j = 0; j < score[i].length-1; j++) {
 				System.out.print("점수 "+(j+1)+" 입력 : ");
 				score[i][j] = sc.nextInt();
 				while(score[i][j]>100 || score[i][j]<0) {
 					System.out.print("다시 입력하세요==> 점수 "+(j+1)+" 입력 : ");
 					score[i][j] = sc.nextInt();
 				}
+				if(score[i][j] > max_sco[i]) {
+					max_sco[i] = score[i][j];
+				}
 			}
 			System.out.println("");
 		}
+		System.out.println("");
+		
+		System.out.println("======================================");
+		System.out.println("순번   점수1    점수2    점수3   최고점   평점");
+		for(int i = 0; i < per; i++) {
+			System.out.print((i+1)+"   ");
+			System.out.printf("%5d    %4d    %4d    %3d",score[i][0],score[i][1],score[i][2],max_sco[i]);
+			System.out.println("");
+		}
+		System.out.println("======================================");
 	}
 
 }
