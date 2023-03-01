41860811af4432bf09c14012dd31270c68c345be
leejisu <ljsgoo0617@gmail.com>
2023-03-01 23:16:53 +0900

    출금 메서드(withdraw) 선언 및 호출

--- CodingTest/src/CodingTestSuhaeng/suhaeng6.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng6.java
@@ -16,6 +16,12 @@ public class suhaeng6 {
 			System.out.print("입금할 금액 : ");
 			acc.deposi(sc.nextInt());
 			acc.balancePrint();
+			System.out.println();
+			
+			System.out.print("출금할 금액 : ");
+			acc.withdraw(sc.nextInt());
+			acc.balancePrint();
+			System.out.println();
 		}
 	}
 
@@ -29,6 +35,13 @@ class Account {
 	}
 	void deposi(int amo) {
 		this.amo += amo;
+	}
+	void withdraw(int amo) {
+		if(this.amo>amo) {
+			this.amo -= amo;
+		} else {
+			System.out.println("출금하려는 계좌의 잔액이 부족하여 출금이 불가능합니다");
+		}
 	}
 	void balancePrint() {
 		System.out.println("계좌번호 "+acc_num+"의 현재 잔액 : "+amo);
