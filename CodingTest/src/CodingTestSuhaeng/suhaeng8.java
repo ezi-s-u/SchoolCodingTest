5bf517eee33267a94dd7cdc0bc211eafcf2bf48d
leejisu <ljsgoo0617@gmail.com>
2023-03-05 00:02:10 +0900

    출력 위치 오류, 누적판매량 계산 오류 해결 (suhaeng8 완성)

--- CodingTest/src/CodingTestSuhaeng/suhaeng8.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng8.java
@@ -16,13 +16,16 @@ public class suhaeng8 {
 			for(int j = 0; j < sale[i].length; j++) {
 				sale[i][j] = sc.nextInt();
 				year_sale[i] += sale[i][j];
-				total += sale[i][j];
 			}
 		}
 		System.out.println("             1사분기   2사분기   3사분기   4사분기  연간판매량  누적판매량");
 		for(int i = 0; i < sale.length; i++) {
 			System.out.print((2019+i)+"년도 : ");
-			System.out.printf("%-8d %-8d %-8d %-8d %-8d %-8d\n",sale[i][0],sale[i][1],sale[i][2],sale[i][3],year_sale[i],total);
+			System.out.printf("%7d %8d %7d %7d %8d",sale[i][0],sale[i][1],sale[i][2],sale[i][3],year_sale[i]);
+			for(int j = 0; j < sale[i].length; j++) {
+				total += sale[i][j];
+			}
+			System.out.printf(" %8d\n",total);
 		}
 	}
 }
