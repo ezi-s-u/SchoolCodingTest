c2d438491a3ea786c2de0adce359cba7b637c24e
leejisu <ljsgoo0617@gmail.com>
2023-03-04 23:40:08 +0900

    연간판매량, 누적판매량 저장

--- CodingTest/src/CodingTestSuhaeng/suhaeng8.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng8.java
@@ -8,11 +8,15 @@ public class suhaeng8 {
 		Scanner sc = new Scanner(System.in);
 		
 		int sale[][] = new int[3][4];
+		int year_sale[] = new int[3];
+		int total = 0;
 		
 		for(int i = 0; i < sale.length; i++) {
 			System.out.print((2019+i)+"³âµµ : ");
 			for(int j = 0; j < sale[i].length; j++) {
 				sale[i][j] = sc.nextInt();
+				year_sale[i] += sale[i][j];
+				total += sale[i][j];
 			}
 		}
 	}
