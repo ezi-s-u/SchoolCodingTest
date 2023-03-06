cded73b55035d69480305889e94163a01a6a8621
leejisu <ljsgoo0617@gmail.com>
2023-03-06 23:25:31 +0900

    출력 오류

--- CodingTest/src/CodingTestSuhaeng/suhaeng10.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng10.java
@@ -6,8 +6,16 @@ public class suhaeng10 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.print("영문자 입력 : ");
 		String str = sc.next();
+		char ch[] = new char[str.length()];
+				
+		for(int i = ch.length-1, j = 0; i >= 0; i--, j++) {
+			ch[j]= str.charAt(i);
+		}
+		
+		System.out.print("변환된 문자 : "+ch.toString());
 	}
 
 }
