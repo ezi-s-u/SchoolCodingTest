bbe05c9a13754f163f9ac3b63c4ed39202638f1f
leejisu <ljsgoo0617@gmail.com>
2023-03-06 23:19:15 +0900

    문자열 입력 받기

--- CodingTest/src/CodingTestSuhaeng/suhaeng10.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng10.java
@@ -1,10 +1,13 @@
 package CodingTestSuhaeng;
 
+import java.util.Scanner;
+
 public class suhaeng10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("영문자 입력 : ");
+		String str = sc.next();
 	}
 
 }
