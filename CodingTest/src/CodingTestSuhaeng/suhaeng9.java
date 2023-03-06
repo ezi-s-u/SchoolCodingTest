7da2631fc04d6b4c77dac232bcb5891e56e64b63
leejisu <ljsgoo0617@gmail.com>
2023-03-06 22:59:27 +0900

    입력 받기 완료

---
+++ CodingTest/src/CodingTestSuhaeng/suhaeng9.java
@@ -0,0 +1,15 @@
+package CodingTestSuhaeng;
+
+import java.util.Scanner;
+
+public class suhaeng9 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("계산하고자 하는 첫번째 수 입력 : ");
+		int n1 = scan.nextInt();
+		System.out.print("계산하고자 하는 두번째 수 입력 : ");
+		int n2 = scan.nextInt();
+	}
+
+}
