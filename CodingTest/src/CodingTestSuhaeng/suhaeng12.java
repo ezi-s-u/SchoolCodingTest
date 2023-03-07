8c71434481cc5ed70c1223610ebb92cdaf2678bd
leejisu <ljsgoo0617@gmail.com>
2023-03-07 23:28:52 +0900

    suhaeng11 건너뛰고 12. 조건 3 완료

---
+++ CodingTest/src/CodingTestSuhaeng/suhaeng12.java
@@ -0,0 +1,12 @@
+package CodingTestSuhaeng;
+
+public class suhaeng12 {
+
+	public static void main(String[] args) {
+		Rectangle t1 = new Rectangle(1,1,3,3);
+		t1.showRectangle();
+		System.out.pritnln("사각형의 둘레: "+t1.getRound());
+		System.out.pritnln("사각형의 넓이: "+t1.getArea());
+	}
+
+}
