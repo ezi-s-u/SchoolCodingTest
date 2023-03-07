4b49289313727192ffa1700f0eeb05f4a9522d08
leejisu <ljsgoo0617@gmail.com>
2023-03-07 23:37:13 +0900

    showRectangle메서드 선언 및 구현

--- CodingTest/src/CodingTestSuhaeng/suhaeng12.java
+++ CodingTest/src/CodingTestSuhaeng/suhaeng12.java
@@ -5,8 +5,8 @@ public class suhaeng12 {
 	public static void main(String[] args) {
 		Rectangle t1 = new Rectangle(1,1,3,3);
 		t1.showRectangle();
-		System.out.pritnln("사각형의 둘레: "+t1.getRound());
-		System.out.pritnln("사각형의 넓이: "+t1.getArea());
+		//System.out.pritnln("사각형의 둘레: "+t1.getRound());
+		//System.out.pritnln("사각형의 넓이: "+t1.getArea());
 	}
 
 }
@@ -18,5 +18,9 @@ class Rectangle {
 		y1 = b;
 		x2 = c;
 		y2 = d;
+	}
+	void showRectangle() {
+		System.out.println("사각형의 좌표는 왼쪽 위 (" + x1 +","+y1+")");
+		System.out.println("사각형의 좌표는 오른쪽 아래 (" + x2 +","+y2+")");
 	}
 }
